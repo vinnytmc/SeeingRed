0a02ac014646135f7f293e1e8cb57f204fcd318e
vinnytmv <valopez@mail.csuchico.edu>
2018-10-22 18:48:52 -0700

    Seeing red

--- main.cpp
+++ main.cpp
@@ -9,26 +9,50 @@ using namespace std;
 int main(int argc, char* argv[])
 {
   Bitmap image;
-  
   string filename;
+  int row;
+  int column;
+  Pixel color;
+
+
   if (argc == 2)
   {
     filename = argv[1];
     image.open(filename);
+    vector <vector <Pixel> > imagePixels;
+
     if(image.isImage() == true)
     {
-      vector <vector <Pixel> > imagePixels = image.toPixelMatrix();
+      imagePixels = image.toPixelMatrix();
       cout<<"kendall.bmp is "<<imagePixels.size()<<" high and "<<imagePixels[0].size()<<" pixels wide\n";
+
+      for(row = 0; row < imagePixels.size(); row++)
+      {
+        for(column = 0; column < imagePixels[0].size(); column++)
+        {
+          color = imagePixels[row][column];
+          color.green = 0;
+          color.blue = 0;
+
+          imagePixels[row][column] = color;
+        }
+      }
+
     }
     else
     {
-     cout<<"Image file must be a bitmap with 24-bit color depth.\n";
+      cout<<"Image file must be a bitmap with 24-bit color depth.\n";
     }
+
+    if(argc != 2)
+    {
+      cout<<"Please specify one image file!\n";
+    }
+
+    image.fromPixelMatrix( imagePixels);
+    image.save("redness.bmp");
+
   }
-  if(argc != 2)
-  {
-    cout<<"Please specify one image file!\n";
-  }
-   
+
   return 0;
 }
